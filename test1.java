57e33918395ebf636a48f46c9810d95fd97b46d2
sinduinnz <53026682+sinduinnz@users.noreply.github.com>
2021-10-07 13:45:04 +1300

    Update test1.java

--- test1.java
+++ test1.java
@@ -5,16 +5,14 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HomePage;
 public class FormTest extends BaseTest {
+  
   @Test
   public void logIntoTestProjectExamplePage () {
-    loginPage.setFullName("Rex Jones II");
-    loginPage.setPassword("12345");
-    HomePage home_page = loginPage.clickLoginButton();
-    formPage.completeForm(
-            "United States",
-            "1234 TestProject",
-            "Rex.Jones@Test4Success.org",
-            "214-225-1234");
+  
+    homePage.setFullName("Rex Jones II");
+    homePage.setPassword("12345");
+    ContactPage contact_page = homePage.clickLoginButton();
+    contact_page.
     Assert.assertTrue(formPage.getConfirmationMessage().equals("Saved"));
   }
 }
